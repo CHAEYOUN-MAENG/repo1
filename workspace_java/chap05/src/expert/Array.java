9009d9fac9323afe8c9efbae0df6166ed5176280
맹채윤 <koung3132@naver.com>
2024-07-18 09:44:26 +0900

    Array, 디버그모드 활용

---
+++ workspace_java/chap05/src/expert/Array.java
@@ -0,0 +1,20 @@
+package expert;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// 문제 1
+		int p = 1;
+		int a[] = new int[10];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = p;
+			System.out.println(p);
+			p++;
+		}
+
+		System.out.println("----------------------");
+		// 문제 2
+
+	}
+
+}
